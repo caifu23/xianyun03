799f4b6a596bc16046f31d329e00c60db6a4d82f
caifu23 <1224758467@qq.com>
2020-02-07 11:25:33 +0800

    Signed-off-by: caifu23 <1224758467@qq.com>

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <h1 class="title">xianyun03</h1>
       <h2 class="subtitle">My striking Nuxt.js project</h2>
       <h3> test-------- </h3>
+      <h4> ++++++++++ </h4>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
