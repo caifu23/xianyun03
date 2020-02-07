f2e7e3abbef975eaad04647a0a71565dc148b449
caifu23 <1224758467@qq.com>
2020-02-07 18:44:53 +0800

    05-首页tab栏搜索框实现切换实现

--- components/header.vue
+++ components/header.vue
@@ -8,15 +8,15 @@
         </nuxt-link>
       </div>
       <!-- 菜单栏  -->
-      <el-row type="flex" class="navs" align="middle" >
-        <nuxt-link to="/">首页</nuxt-link>
-        <nuxt-link to="/post">旅游攻略</nuxt-link>
-        <nuxt-link to="/hotel">酒店</nuxt-link>
-        <nuxt-link to="/air">国内机票</nuxt-link>
+      <el-row type="flex" class="navs" align="middle">
+        <nuxt-link to="/"  exact-active-class="active">首页</nuxt-link>
+        <nuxt-link to="/post" exact-active-class="active">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel" exact-active-class="active">酒店</nuxt-link>
+        <nuxt-link to="/air" exact-active-class="active">国内机票</nuxt-link>
       </el-row>
       <!-- 登录/用户信息  -->
       <el-row type="flex" align="middle">
-          <!-- 消息 -->
+        <!-- 消息 -->
         <el-dropdown class="message">
           <span class="el-dropdown-link">
             <i class="el-icon-bell"></i>
@@ -27,10 +27,10 @@
             <el-dropdown-item>消息</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-          <!-- 如果用户已登录，则显示 用户信息-->
+        <!-- 如果用户已登录，则显示 用户信息-->
         <el-dropdown v-if="loginStatu" class="account">
           <span class="el-dropdown-link">
-            <img src="http://157.122.54.189:9095/assets/images/avatar.jpg" alt="">
+            <img src="http://157.122.54.189:9095/assets/images/avatar.jpg" alt />
             地球发动机
             <i class="el-icon-caret-bottom"></i>
           </span>
@@ -40,9 +40,7 @@
           </el-dropdown-menu>
         </el-dropdown>
         <!-- 没有登录，则显示登录/注册 -->
-        <nuxt-link to="/user/login" class="login-link" v-else>
-            登录 / 注册
-        </nuxt-link>
+        <nuxt-link to="/user/login" class="login-link" v-else>登录 / 注册</nuxt-link>
       </el-row>
     </el-row>
   </header>
@@ -50,61 +48,72 @@
 
 <script>
 export default {
-    data () {
-        return {
-            loginStatu: true
-        }
-    }
-}
+  data() {
+    return {
+      loginStatu: true
+    };
+  }
+};
 </script>
 
 <style lang="less">
+// 激活时的样式
+// a {
+  .active {
+    color: #fff!important;
+    background-color: #409eff;
+  }
+  
+// }
+
 .main {
-    margin: 0 auto;
-    width: 1000px;
-    height: 60px;
-    // logo 
-    .logo {
-        margin-right: 25px;
-        padding-top: 8px;
-        width: 156px;
-        img {
-            display: block;
-            width: 100%;
-        }
+  margin: 0 auto;
+  width: 1000px;
+  height: 60px;
+  // logo
+  .logo {
+    margin-right: 25px;
+    padding-top: 8px;
+    width: 156px;
+    img {
+      display: block;
+      width: 100%;
     }
-    // 菜单栏
-    .navs {
-        flex: 1;
-        font-size: 15px;
-        a {
-            display: block;
-            padding: 0 20px;
-            box-sizing: border-box;
-            height: 60px;
-            line-height: 60px;
-            &:hover, &:focus, &:active {
-                color: #409eff;
-                border-bottom: 4px solid #409eff;
-            }
-        }
+  }
+  // 菜单栏
+  .navs {
+    flex: 1;
+    font-size: 15px;
+    a {
+      display: block;
+      padding: 0 20px;
+      box-sizing: border-box;
+      height: 60px;
+      line-height: 60px;
+      &:hover,
+      &:focus,
+      &:active {
+        color: #409eff;
+        border-bottom: 4px solid #409eff;
+      }
     }
-    // 登录 / 个人信息
-    .message {
-        margin-right: 15px;
+  }
+  // 登录 / 个人信息
+  .message {
+    margin-right: 15px;
+  }
+  .account {
+    margin-left: 20px;
+    img {
+      vertical-align: middle;
+      border-radius: 50%;
+      width: 32px;
+      height: 32px;
     }
-    .account {
-        margin-left: 20px;
-        img {
-            vertical-align: middle;
-            border-radius: 50%;
-            width: 32px;
-            height: 32px;
-        }
-    }
-    .login-link {
-        font-size: 13px;
-        color: red;
-    }
+  }
+  .login-link {
+    font-size: 13px;
+    color: red;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <!-- 轮播图（走马灯） -->
     <el-carousel :interval="5000" arrow="always">
       <el-carousel-item v-for="(item,index) in banners" :key="index">
         <div
@@ -8,6 +9,24 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+
+    <!-- tab栏：搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar" >
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span v-for="(item, index) in searchList" :key="index" :class="{ active: index === current}" @click="tabClick(index)" >
+            <i> {{ item.text }} </i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="searchList[current].placeholder"/>
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,24 +34,51 @@
 export default {
   data() {
     return {
-      banners: []
-    }
+      current: 0,
+      banners: [],
+      searchList: [
+        {
+          text: '攻略',
+          placeholder: '搜索城市',
+          pageUrl: '/post?city='
+        },
+        {
+          text: '酒店',
+          placeholder: '请输入城市搜索酒店',
+          pageUrl: '/hotel?city='
+        },
+        {
+          text: '机票',
+          placeholder: '',
+          pageUrl: '/air'
+        }
+      ]
+    };
   },
   mounted() {
     // console.log(this.$axios.defaults.baseURL)
     // 获取轮播图数据
-    this.getBanners()
+    this.getBanners();
   },
   methods: {
     // 获取轮播图
     async getBanners() {
       let resBanner = await this.$axios({
         url: "/scenics/banners"
-      })
+      });
       // console.log(resBanner)
       if (resBanner.data.data) {
-        let { data } = resBanner.data
-        this.banners = data
+        let { data } = resBanner.data;
+        this.banners = data;
+      }
+    },
+    // 切换tab
+    tabClick(index) {
+      this.current = index
+
+      // 如果点击的是 机票，则跳转
+      if(index === 2) {
+        this.$router.push(this.searchList[index].pageUrl)
       }
     }
   }
@@ -47,6 +93,95 @@ export default {
     width: 100%;
     height: 100%;
     background-size: contain contain;
+  }
+}
+
+.banner-content {
+  z-index: 9;
+  width: 1000px;
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  margin-left: -500px;
+  border-top: 1px transparent solid;
+
+  .search-bar {
+    width: 552px;
+    margin: 0 auto;
+  }
+
+  .search-tab {
+    .active {
+      i {
+        color: #333;
+      }
+      &::after {
+        background: #eee;
+      }
+    }
+
+    span {
+      width: 82px;
+      height: 36px;
+      display: block;
+      position: relative;
+      margin-right: 8px;
+      cursor: pointer;
+
+      i {
+        position: absolute;
+        z-index: 2;
+        display: block;
+        width: 100%;
+        height: 100%;
+        line-height: 30px;
+        text-align: center;
+        color: #fff;
+      }
+
+      &:after {
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: block;
+        content: "";
+        width: 100%;
+        height: 100%;
+        border: 1px rgba(255, 255, 255, 0.2) solid;
+        border-bottom: none;
+        transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+        transform-origin: bottom left;
+        background: rgba(0, 0, 0, 0.5);
+        border-radius: 1px 2px 0 0;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  .search-input {
+    width: 550px;
+    height: 46px;
+    background: #fff;
+    border-radius: 0 4px 4px 4px;
+    border: 1px rgba(255, 255, 255, 0.2) solid;
+    border-top: none;
+    box-sizing: unset;
+
+    input {
+      flex: 1;
+      height: 20px;
+      padding: 13px 15px;
+      outline: none;
+      border: 0;
+      font-size: 16px;
+    }
+
+    .el-icon-search {
+      cursor: pointer;
+      font-size: 22px;
+      padding: 0 10px;
+      font-weight: bold;
+    }
   }
 }
 </style>
