201943e7dabfd88d4aa2c0efcd81b1794e26467b
caifu23 <1224758467@qq.com>
2020-02-13 17:04:35 +0800

    20-机票列表页头部,查询关键词获取

---
+++ components/air/flightsListHead.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list-head">
+      <el-row type="flex">
+          <el-col span="5">航空信息</el-col>
+          <el-col span="7">起飞时间</el-col>
+          <el-col span="7">到达时间</el-col>
+          <el-col span="5">价格</el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.list-head {
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    padding: 10px;
+    text-align: center;
+    font-size: 13px;
+    color: #666666;
+    background-color: #f6f6f6;
+}
+</style>
---
+++ pages/air/flights.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flights container">
+    <el-row type="flex" justify-content="space-between">
+      <!-- 机票列表 -->
+      <el-col class="flights-list">
+        <!-- 机票筛选条件 -->
+        <!-- 机票信息列表 -->
+        <div class="flights-content">
+            <!-- 机票头部 -->
+            <FlightsListHead></FlightsListHead>
+        </div>
+        列去
+      </el-col>
+
+      <!-- 侧边栏: 历史查询 -->
+      <el-col class="flights-history">列2</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import FlightsListHead from '@/components/air/flightsListHead'
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    // 获取查询关键字
+    getQuery() {
+      const query = this.$route.query;
+      console.log(query);
+    }
+  },
+  mounted() {
+    // 获取查询关键字
+    this.getQuery();
+  },
+  components: {
+    FlightsListHead
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+}
+.flights {
+  .flights-list {
+    width: 745px;
+    margin-right: 20px;
+  }
+  .flights-history {
+    width: 240px;
+  }
+}
+</style>
