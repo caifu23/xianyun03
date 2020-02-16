6b0ffb681b3089c197b705955532f949c4250078
caifu23 <1224758467@qq.com>
2020-02-16 17:45:15 +0800

    47-store历史记录截取判断

--- store/air.js
+++ store/air.js
@@ -18,7 +18,10 @@ export const mutations = {
         // state.history.unshift(val)
         // 方法二：通过数组length来截取，限制长度
         state.history.unshift(val)
-        state.history.length = 5
+        if(state.history.length >= 5) {
+            state.history.length = 5
+        }
+        
     },
     // 添加订单数据
     setInfoData(state, data) {
